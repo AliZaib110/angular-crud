2aa6b9e04fa9d7f75711eb5c4cc55b61da5df2ab
alizaib110 <alizaibbuttar9@gmail.com>
2024-10-10 22:47:11 +0500

    crud in angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,131 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'CRUD';
+  @ViewChild('helloModal') helloEl?: ElementRef;
+  modal?: bootstrap.Modal;
+  selectedName: any;
+  selectedDescription: any;
+  selectedCategoryId: any;
+  ItemsList = [
+    {
+      id: 1,
+      name: 'Item 1',
+      description: 'description A',
+      category: 'Books',
+    },
+    {
+      id: 2,
+      name: 'Item 2',
+      description: 'description B',
+      category: ['Clothing'],
+    },
+    {
+      id: 3,
+      name: 'Item 3',
+      description: 'description C',
+      category: ['Electronics', 'Clothing'],
+    },
+    {
+      id: 4,
+      name: 'Item 4',
+      description: 'description D',
+      category: ['Books', 'Other'],
+    },
+  ];
+  categoryList = [
+    { id: 11, name: 'Electronics' },
+    { id: 22, name: 'Clothing' },
+    { id: 33, name: 'Books' },
+    { id: 44, name: 'Other' },
+  ];
+  rId: number = 0;
+
+  ngAfterViewInit() {
+    this.modal = new bootstrap.Modal(this.helloEl?.nativeElement, {});
+  }
+
+  trigger() {
+    this.modal?.toggle();
+  }
+  closeModal() {
+    this.modal?.hide();
+  }
+
+  // upsert() {
+  //   debugger;
+  //   if (
+  //     this.selectedName == '' &&
+  //     this.selectedDescription == '' &&
+  //     this.selectedCategoryId == ''
+  //   ) {
+  //     return;
+  //   }
+  //   console.log(this.rId, 'rid');
+  //   let lastId = this.ItemsList?.length;
+  //   const newItem: any = {
+  //     id: this.rId ? this.rId : ++lastId,
+  //     name: this.selectedName,
+  //     description: this.selectedDescription,
+  //     category: this.selectedCategoryId,
+  //   };
+  //   this.ItemsList.push(newItem);
+  // }
+  upsert() {
+    debugger;
+    if (
+      this.selectedName === '' &&
+      this.selectedDescription === '' &&
+      this.selectedCategoryId === ''
+    ) {
+      return;
+    }
+    let itemIndex = this.ItemsList.findIndex((item) => item.id === this.rId);
+    const newItem = {
+      id: this.rId ? this.rId : this.ItemsList.length + 1,
+      name: this.selectedName,
+      description: this.selectedDescription,
+      category: this.selectedCategoryId,
+    };
+    if (itemIndex !== -1) {
+      this.ItemsList[itemIndex] = newItem;
+    } else {
+      this.ItemsList.push(newItem);
+    }
+  }
+
+  reSetValues() {
+    this.selectedName = '';
+    this.selectedDescription = '';
+    this.selectedCategoryId = '';
+  }
+
+  editRecord(id: number) {
+    this.rId = id;
+    let itemToEdit = this.ItemsList.find((item) => item.id === id);
+    if (itemToEdit) {
+      this.selectedName = itemToEdit.name;
+      this.selectedDescription = itemToEdit.description;
+      this.selectedCategoryId = itemToEdit.category;
+    }
+    this.trigger();
+  }
+
+  deleteRecord(id: number) {
+    let index = this.ItemsList.findIndex((item) => item.id === id);
+    if (index !== -1) {
+      this.ItemsList.splice(index, 1);
+    }
+  }
 }
